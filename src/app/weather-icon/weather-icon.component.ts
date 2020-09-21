006b60c420c2c720dfba9a4bd55465a61d2acae6
Evren Ceyhan <evren@ceyhan.io>
2020-09-21 21:41:06 +0200

    weather-icon conditions refactored

--- src/app/weather-icon/weather-icon.component.ts
+++ src/app/weather-icon/weather-icon.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input } from '@angular/core';
-
-type Condition = 'sunny' | 'cloudy' | 'rainy' | 'windy' | 'stormy' | 'snowy';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { WeatherCondition } from '../models';
 
 @Component({
   selector: 'app-weather-icon',
   templateUrl: './weather-icon.component.html',
   styleUrls: ['./weather-icon.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class WeatherIconComponent  {
+export class WeatherIconComponent {
   @Input()
-  condition: Condition = 'sunny';
+  condition: WeatherCondition;
 }
