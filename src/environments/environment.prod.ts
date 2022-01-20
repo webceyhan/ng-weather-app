bb9940cffa9f071714cf76ce7f08acbb6d63ba9f
Evren Ceyhan <evren@ceyhan.io>
2022-01-20 23:35:10 +0300

    api-key updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUnit: 'metric',
-  apiKey: '64295d512d4fc2f29df6fdba3e120e94',
+  apiKey: '591dacc7a16f386b0ac67b3d4b901aad',
   apiUrl: 'https://api.openweathermap.org/data/2.5',
 };
