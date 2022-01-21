9df1b605cd821c033a15a57ebef5d071e24f5d95
Evren Ceyhan <evren@ceyhan.io>
2022-01-21 04:40:15 +0300

    code cleanup

--- server.js
+++ server.js
@@ -4,57 +4,52 @@ const path = require("path");
 
 const app = express();
 
-const rootDir = __dirname + "/dist/weather-app";
-
-app.use(express.static(rootDir));
-
+// API /////////////////////////////////////////////////////////////////////////////////////////////
 // Workound: Same-Origin-Policy
 // HTTP Ajax calls are not allowed from HTTPS host so we bypass the calls
 // through this local server instead of calling it directly from within Angular.
+
+const GEOCITY_URL =
+  "http://geodb-free-service.wirefreethought.com/v1/geo/cities";
+const OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5";
+
+const openWeatherParams = {
+  units: "metric",
+  appid: '591dacc7a16f386b0ac67b3d4b901aad',
+};
+
 app.get("/api/weather", async (req, res) => {
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-
-  url.search = new URLSearchParams({
-    units: "metric",
-    appid: "591dacc7a16f386b0ac67b3d4b901aad",
-    q: req.query.city,
-  });
+  const q = req.query.city;
+  const url = new URL(`${OPENWEATHER_URL}/weather`);
+  url.search = new URLSearchParams({ ...openWeatherParams, q });
 
   const response = await fetch(url);
   res.json(await response.json());
 });
 
-// Workound: Same-Origin-Policy
-// HTTP Ajax calls are not allowed from HTTPS host so we bypass the calls
-// through this local server instead of calling it directly from within Angular.
 app.get("/api/forecast", async (req, res) => {
-  const url = new URL("https://api.openweathermap.org/data/2.5/forecast");
-
-  url.search = new URLSearchParams({
-    units: "metric",
-    appid: "591dacc7a16f386b0ac67b3d4b901aad",
-    q: req.query.city,
-  });
+  const q = req.query.city;
+  const url = new URL(`${OPENWEATHER_URL}/forecast`);
+  url.search = new URLSearchParams({ ...openWeatherParams, q });
 
   const response = await fetch(url);
   res.json(await response.json());
 });
 
-// Workound: Same-Origin-Policy
-// HTTP Ajax calls are not allowed from HTTPS host so we bypass the calls
-// through this local server instead of calling it directly from within Angular.
 app.get("/api/cities", async (req, res) => {
   const { namePrefix } = req.query;
-
-  const url = new URL(
-    "http://geodb-free-service.wirefreethought.com/v1/geo/cities"
-  );
-
+  const url = new URL(GEOCITY_URL);
   url.search = new URLSearchParams({ namePrefix });
 
   const response = await fetch(url);
   res.json(await response.json());
 });
+
+// STATIC //////////////////////////////////////////////////////////////////////////////////////////
+
+const rootDir = __dirname + "/dist/weather-app";
+
+app.use(express.static(rootDir));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(rootDir + "/index.html"));
