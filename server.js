91c89083a0453fcab9c8a3b294e0f36b2a4f896e
Evren Ceyhan <evren@ceyhan.io>
2022-01-21 03:00:33 +0300

    heroku build and server added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const rootDir = __dirname + "/dist/weather-app";
+
+app.use(express.static(rootDir));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(rootDir + "/index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
